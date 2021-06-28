f03c408de1af6ed466a6a44bb13c602620d0005d
Nishkarsh Dubb <nishdubb11@gmail.com>
2021-06-28 12:13:30 +0530

    Update animated-counter.js

--- js/animated-counter.js
+++ js/animated-counter.js
@@ -1,5 +1,5 @@
 const counters = document.querySelectorAll('.counter');
-const speed = 10000; // The lower the slower
+const speed = 200; // The lower the slower
 
 counters.forEach((counter) => {
   const updateCount = () => {
